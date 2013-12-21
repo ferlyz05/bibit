1bd1fa05e5c31919785b86d42c348ff9be92d6a9
ferlyz05 <lombok.primitive@gmail.com>
2013-12-22 00:24:30 +0700

    Update console.php

--- protected/config/console.php
+++ protected/config/console.php
@@ -44,8 +44,8 @@ return array(
         'composer.callbacks' => array(
             // args for Yii command runner
             'yiisoft/yii-install' => array('yiic', 'webapp', dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'),
-            'post-update' => array('yiic', 'migrate'),
-            'post-install' => array('yiic', 'migrate'),
+            //'post-update' => array('yiic', 'migrate'),
+            //'post-install' => array('yiic', 'migrate'),
         ),
     ),        
 	'commandMap' => array(
