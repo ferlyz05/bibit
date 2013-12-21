d63e5e1adb7c95d7bdb69c93665eed66163eede5
ferlyz05 <lombok.primitive@gmail.com>
2013-12-22 00:03:10 +0700

    post-install command

--- protected/config/console.php
+++ protected/config/console.php
@@ -40,11 +40,38 @@ return array(
 			),
 		),
 	),
+	'params' => array(
+        'composer.callbacks' => array(
+            // args for Yii command runner
+            'yiisoft/yii-install' => array('yiic', 'webapp', dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'),
+            'post-update' => array('yiic', 'migrate'),
+            'post-install' => array('yiic', 'migrate'),
+        ),
+    ),        
 	'commandMap' => array(
 		'migrate' => array(
+			 // alias of the path where you extracted the zip file
+            'class' => 'vendor.yiiext.migrate-command.EMigrateCommand',
+            // this is the path where you want your core application migrations to be created
+            'migrationPath' => 'application.migrations',
+            // the name of the table created in your database to save versioning information
+            'migrationTable' => 'migration',
+            // the application migrations are in a pseudo-module called "core" by default
+            'applicationModuleName' => 'core',
+            // define all available modules (if you do not set this, modules will be set from yii app config)
 			'modulePaths' => array(
 				'user' => 'application.vendor.mishamx.yii-user.migrations',
 			),
-		)
+            // you can customize the modules migrations subdirectory which is used when you are using yii module config
+            'migrationSubPath' => 'migrations',
+            // here you can configure which modules should be active, you can disable a module by adding its name to this array
+            'disabledModules' => array(
+				#'admin', 'anOtherModule', // ...
+            ),
+            // the name of the application component that should be used to connect to the database
+            'connectionID' => 'db',
+			// alias of the template file used to create new migrations
+			#'templateFile' => 'system.cli.migration_template',
+        ),
 	)
 );
